f58ce7f11023873fafaf6b03efef409fb46f2af5
techdeus <sullivan.marlon@gmail.com>
2018-09-24 14:32:38 -0700

    minor changes

--- src/Bee.js
+++ src/Bee.js
@@ -1,6 +1,6 @@
 class Bee extends Grub {
  	constructor() {
-  		super();
+  		super(); // call the Super function before initializing variables in the subclass
   		this.age = 5;
   		this.color = 'yellow';
   		this.job = 'Keep on growing';
